9840f3a86bd5f8badd2d797d7ca1687b54ddf4c5
Emily Chao <elchao96@live.com>
2015-09-06 00:18:42 -0500

    script cleanup

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,12 +7,3 @@ $(document).ready(function() {
         event.preventDefault();
     });
 });
-
-//jQuery to collapse the navbar on scroll
-/*$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});*/
